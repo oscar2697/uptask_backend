0c8789abad55c191d973280e8049b91c14a092fa
Oscar Lindo <77596284+oscar2697@users.noreply.github.com>
2024-07-31 00:15:27 -0500

    "Added express-validator dependency and implemented input validation for project creation route"

--- src/routes/ProjectRoutes.ts
+++ src/routes/ProjectRoutes.ts
@@ -1,9 +1,21 @@
 import { Router } from "express";
 import { ProjectController } from "../controller/ProjectController";
+import { body } from "express-validator";
+import { handleInputErrors } from "../middleware/validation";
 
 const router = Router()
 
-router.post('/', ProjectController.createProject)
+router.post('/',
+    body('projectName')
+        .notEmpty().withMessage('The Project Name is required'),
+    body('clientName')
+        .notEmpty().withMessage('The Client Name is required'),
+    body('description')
+        .notEmpty().withMessage('The Description is required'),
+    handleInputErrors,
+    ProjectController.createProject
+)
+
 router.get('/', ProjectController.getAllProjects)
 
 export default router
