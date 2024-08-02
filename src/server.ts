2c58994ec246b2076bd7e2d772252ed2106a9e95
Oscar Lindo <77596284+oscar2697@users.noreply.github.com>
2024-08-02 17:19:43 -0500

    Install morgan

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import cors from 'cors'
 import { connectDB } from "./config/db";
 import projectRoutes from './routes/ProjectRoutes'
 import { corsConfig } from "./config/cors";
+import morgan from "morgan";
 
 dotenv.config()
 connectDB()
@@ -11,8 +12,13 @@ connectDB()
 const app = express()
 app.use(cors(corsConfig))
 
+//Logging
+app.use(morgan('dev'))
+
+//Read the form's data
 app.use(express.json())
 
+//Routes
 app.use('/api/projects', projectRoutes)
 
 export default app
