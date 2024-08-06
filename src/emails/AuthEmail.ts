26232832467636b617ab331317007582e7ca3e28
Oscar Lindo <77596284+oscar2697@users.noreply.github.com>
2024-08-06 03:32:39 -0500

    Changes on the htmo AuthEmail

--- src/emails/AuthEmail.ts
+++ src/emails/AuthEmail.ts
@@ -14,10 +14,11 @@ export class AuthEmail {
             subject: 'UpTask - Verify Your Email',
             text: 'UpTask -  Verify Your Email',
             html: `
-                <p>Hi: ${user.name}, You have created an Account in UpTask. Just confirm your Email to Start</p>
-                <p>Click in the next Link: </p>
-                <a href="">Confirm Your Account</a>
-                <p>Enter Code: <b>${user.token}</b></p>
+                <p>Hi: ${user.name}, Thanks for signing up with UpTask! </p>
+                <p>You must follow this link within 10 minutes of registration to activate your account:</p>
+                <a href="${process.env.FRONTEND_URL}/auth/confirm">Confirm Your Account</a>
+                <p>Enter this verification code to sign up: <b>${user.token}</b></p>
+                <p>Don't share it with anyone</p>
                 <p>This Token expires in 10 minutes</p>
             `
         })
